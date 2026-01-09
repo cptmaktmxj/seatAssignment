c638ba590a7e677384b1fac3d954e2b83e802370
cptmaktmxj <158984411+cptmaktmxj@users.noreply.github.com>
2026-01-09 20:40:08 +0900

    modal section 나누기

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App(){
       </main>
       <Footer />
 
-      {isModalOpen && (<Modal/>)}
+      {isModalOpen && (<modalOverlay/>)}
 
     </div>
   )
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,27 +1,37 @@
-<input value="엑셀파일 업로드" type="file" onChange={setShowFiles}/>
 
-<label for "add-student">학생 수동 추가<label/>
-
-<form>
-    <div id="add-student">
-        <input type="text" name="name" placeholder="이름"/>
-        <input type="text" name="studentId" placeholder="학번"/>
-        <select name="gender" onChange={if }>
-            <option value="M">남</option>
-            <option value="F">여</option>
-        </select>
-    </div>
-    <button value="추가"></button>
-</form>
-
-<table>
-    //학생 명단 + 삭제 기능
-</table>
 
 function Modal() {
     return(
-       <div className="Modal">
+        <div className="modalOverlay" onClick={onClose}>
+            <div className="Modal" onClick={(e) => e.stopPropagation()}>
 
-        </div> 
+                <div className="section">
+                    <input value="엑셀파일 업로드" type="file" onChange={setShowFiles}/>
+                </div>
+
+                <div className="section">
+                    <label for "add-student">학생 수동 추가<label/>
+                    <form>
+                        <div id="add-student">
+                            <input type="text" name="name" placeholder="이름"/>
+                            <input type="text" name="studentId" placeholder="학번"/>
+                            <select name="gender" onChange={if }>
+                                <option value="M">남</option>
+                                <option value="F">여</option>
+                            </select>
+                        </div>
+                        <button value="추가"></button>
+                    </form>
+                </div>
+
+                <div className="section">
+                    <h4>학생 명단</h4>
+                    <table>
+                        
+                    </table> 
+                </div>
+
+            </div>
+        </div>
     )
 }
