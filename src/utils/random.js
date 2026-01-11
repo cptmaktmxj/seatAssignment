abdc3abd1f68cf0d33e520d43c9b409d580dc2a4
cptmaktmxj <158984411+cptmaktmxj@users.noreply.github.com>
2026-01-12 04:15:04 +0900

    랜덤 배치 로직 작성. 수정 필요

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,3 +1,9 @@
 export function random(seats,students){
-    return //랜덤배치한 새로운 학생 배열을 생성하고 리턴
+    const filteredSeats=seats.filter(seat => !(seat.fixed))
+    const shuffledStudents=[...students].sort(() => Math.random() - 0.5);
+    return(
+        Array.from( , (idx) => (shuffledStudents[idx],filteredSeats[idx]))
+        //suff를 fil에 하나씩 대응시키자.
+    )
+    //랜덤배치한 새로운 학생 배열을 생성하고 (seat,student)의 배열을 리턴
 }
